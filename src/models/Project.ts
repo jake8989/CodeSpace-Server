094060c6d8116c6d48ec3a4389f528f7251fa08c
jake8989 <jjk848798@gmail.com>
2024-05-31 15:16:36 +0530

    steps:xy

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,39 +1,39 @@
 import mongoose, { mongo } from 'mongoose';
-interface UserType {
-	project_id: string;
-	owner_id: string;
-	project_name: string;
-	viewers: string[];
-	collaborators: string[];
-	time: string;
+interface ProjectType {
+  project_id: string;
+  owner_id: string;
+  project_name: string;
+  viewers: string[];
+  collaborators: string[];
+  time: string;
 }
 const Project = new mongoose.Schema(
-	{
-		project_id: { type: String, required: true },
-		owner_id: {
-			type: String,
-			required: true,
-		},
-		project_name: {
-			type: String,
-			required: true,
-		},
-		project_description: {
-			type: String,
-		},
-		viewers: {
-			type: [String],
-			default: [],
-		},
-		collaborators: {
-			type: [String],
-			default: [],
-		},
-		time: {
-			type: String,
-			required: true,
-		},
-	},
-	{ timestamps: true }
+  {
+    project_id: { type: String, required: true },
+    owner_id: {
+      type: String,
+      required: true,
+    },
+    project_name: {
+      type: String,
+      required: true,
+    },
+    project_description: {
+      type: String,
+    },
+    viewers: {
+      type: [String],
+      default: [],
+    },
+    collaborators: {
+      type: [String],
+      default: [],
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
-export default mongoose.model<UserType>('Project', Project);
+export default mongoose.model<ProjectType>('Project', Project);
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -7,46 +7,73 @@ import * as admin from 'firebase-admin';
 import jwt from 'jsonwebtoken';
 import Project from '../models/Project';
 // import { nanoid } from 'nanoid';
+// import ProjectType from '../models/Project';
 router.post(
-	'/create-new-project',
-	async (req: express.Request, res: express.Response) => {
-		const { project_id, owner_id, project_name, project_description, time } =
-			req.body;
-		///an user cannot make same name projects
-		try {
-			const project = {
-				project_id,
-				owner_id,
-				project_name,
-				project_description,
-				time,
-			};
-			const newProject = new Project(project);
-			newProject
-				.save()
-				.then(() => {
-					return res
-						.status(200)
-						.json({ newProject, message: 'Project Created Succefully' });
-				})
-				.catch(() => {
-					return res.status(400).json({ message: 'Something Went Wrong' });
-				});
-		} catch (error) {
-			return res.status(500).json({ message: 'Server Error\n' });
-		}
-	}
+  '/create-new-project',
+  async (req: express.Request, res: express.Response) => {
+    const { project_id, owner_id, project_name, project_description, time } =
+      req.body;
+    ///an user cannot make same name projects
+
+    // console.log(allProjects);
+    // let exists = 0;
+    // Project.find({ owner_id: owner_id })
+    //   .then((projects) => {
+    //     // console.log(projects);
+    //     projects.forEach((project) => {
+    //       if (project.project_name == project_name) {
+    //         console.log('Found');
+    //         exists = 1;
+    //       }
+    //       if (exists == 1) {
+    //         return res
+    //           .status(403)
+    //           .json({ message: 'Project With Same Name Exists' });
+    //       }
+    //       // return;
+    //     });
+    //   })
+    //   .catch((err) => {
+    //     return res.status(502).json({ message: 'Error From Project Route' });
+    //     // console.log(err);
+    //   });
+    // console.log(exists);
+
+    try {
+      const project = {
+        project_id,
+        owner_id,
+        project_name,
+        project_description,
+        time,
+      };
+
+      const newProject = new Project(project);
+      newProject
+        .save()
+        .then(() => {
+          return res
+            .status(200)
+            .json({ newProject, message: 'Project Created Succefully' });
+        })
+        .catch(() => {
+          return res.status(400).json({ message: 'Something Went Wrong' });
+        });
+    } catch (error) {
+      return res.status(500).json({ message: 'Server Error\n' });
+    }
+  }
 );
 router.get(
-	'/get-all-projects',
-	async (req: express.Request, res: express.Response) => {
-		try {
-			const owner_id = req.query.owner_id;
-			const allProjects = await Project.find({ owner_id: owner_id });
-			return res.status(200).json({ allProjects });
-		} catch (error) {
-			return res.status(400).json({ message: 'Server Error' });
-		}
-	}
+  '/get-all-projects',
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const owner_id = req.query.owner_id;
+      const allProjects = await Project.find({ owner_id: owner_id });
+      return res.status(200).json({ allProjects });
+    } catch (error) {
+      return res.status(400).json({ message: 'Server Error' });
+    }
+  }
 );
 export default router;
